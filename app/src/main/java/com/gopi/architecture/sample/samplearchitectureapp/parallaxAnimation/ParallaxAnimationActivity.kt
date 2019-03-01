8cd941b91e8f4128c5bf74b3b08b9145ffb536f8
gopikrishna <gopi@hike.in>
2019-03-01 19:09:02 +0530

    optimized for performance for not setting the alpha again n again.

--- app/src/main/java/com/gopi/architecture/sample/samplearchitectureapp/parallaxAnimation/ParallaxAnimationActivity.kt
+++ app/src/main/java/com/gopi/architecture/sample/samplearchitectureapp/parallaxAnimation/ParallaxAnimationActivity.kt
@@ -2,7 +2,6 @@ package com.gopi.architecture.sample.samplearchitectureapp.parallaxAnimation
 
 import android.graphics.Point
 import android.os.Bundle
-import android.support.v4.view.ViewCompat
 import android.support.v4.widget.NestedScrollView
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
@@ -36,6 +35,7 @@ class ParallaxAnimationActivity : AppCompatActivity() {
 
              }*/
         var point = Point()
+        var previousPercent: Float = -1f
         var profileNameHeight = -1
         var imageParentHeight = -1
         var crossNameHeight = -1
@@ -54,20 +54,26 @@ class ParallaxAnimationActivity : AppCompatActivity() {
         profileNestedScrollView.scrollTo(0, 0)
         profileNestedScrollView.setOnScrollChangeListener() { nestedScrollView: NestedScrollView?, scrollX: Int, scrollY: Int, oldScrollX: Int, oldScrollY: Int ->
             Log.i("GopiLog", "scrolled with scrollY = $scrollY  && oldScrollY = $oldScrollY  ########## $point   with height $profileNameHeight  ;;;; ${imageParentHeight}")
-            if (scrollY >= imageParentHeight && scrollY <= crossNameHeight) {
+            if (scrollY in imageParentHeight..crossNameHeight) {
                 val diff = crossNameHeight - scrollY
-                val percent = 1 - (diff.toFloat().div(profileNameHeight))
-//                titleNameHeader.text = percent.toString()
-                titleNameHeader.alpha = percent
-                titleNameHeader.y = titleNameHeaderExtremePosition - percent*((titleNameHeaderExtremePosition - titleNameHeaderPositionInCenter))
+                var percent = (1 - (diff.toFloat().div(profileNameHeight)))
+                percent = Math.round(percent * 100).toFloat()/100
+                if(previousPercent != percent) {  //optimization only when current percent != previous update it.
+                    titleNameHeader.text = percent.toString()
+                    titleNameHeader.alpha = percent
+                    titleNameHeader.y = titleNameHeaderExtremePosition - percent * ((titleNameHeaderExtremePosition - titleNameHeaderPositionInCenter))
+                    previousPercent = percent
+                }
             } else if (scrollY > crossNameHeight) {
 //                titleNameHeader.text = "crossed"
                 titleNameHeader.alpha = 1f
+                previousPercent = 1f
                 titleNameHeader.y = titleNameHeaderPositionInCenter
             } else {
 //                titleNameHeader.text = "notstarted"
                 titleNameHeader.alpha = 0f
-                titleNameHeader.y = titleNameHeaderExtremePosition
+                previousPercent = 0f
+//                titleNameHeader.y = titleNameHeaderExtremePosition
             }
         }
     }
